15c3200630b8d9bdb83538723cfcada33d1aa054
anthon-cmyk <anthon@cleansearch.ai>
2025-11-24 17:06:12 +0200

    Fixed build issue;

--- src/modules/google-ads/google-ads.module.ts
+++ src/modules/google-ads/google-ads.module.ts
@@ -5,11 +5,12 @@ import { GoogleAdsSyncService } from './google-ads-sync.service';
 import { DrizzleModule } from '../drizzle/drizzle.module';
 import { SupabaseModule } from '../supabase/supabase.module';
 import { GoogleAuthModule } from '../google-auth/google-auth.module';
+import { GoogleAdsFullSyncService } from './google-ads-full-sync.service';
 
 @Module({
   imports: [DrizzleModule, SupabaseModule, forwardRef(() => GoogleAuthModule)],
   controllers: [GoogleAdsController],
-  providers: [GoogleAdsService, GoogleAdsSyncService],
-  exports: [GoogleAdsService, GoogleAdsSyncService],
+  providers: [GoogleAdsService, GoogleAdsSyncService, GoogleAdsFullSyncService],
+  exports: [GoogleAdsService, GoogleAdsSyncService, GoogleAdsFullSyncService],
 })
 export class GoogleAdsModule {}
