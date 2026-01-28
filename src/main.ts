e9b252744989b68ddbde115cc8ff080656865674
anthon-cmyk <anthon@cleansearch.ai>
2026-01-28 08:56:30 +0200

    chore: host update

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
   });
 
-  await app.listen(process.env.PORT ?? 4000);
+  await app.listen(process.env.PORT ?? 4000, '0.0.0.0');
 }
 bootstrap();
